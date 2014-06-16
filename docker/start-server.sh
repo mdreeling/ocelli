fe71a90fdc6b836b32faebbde7e1ee6860561ac2
Michael Dreeling <mdreeling@gmail.com>
2014-06-15 22:09:19 -0700

    Create start-server.sh

---
+++ docker/start-server.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+sudo service nginx start
+sudo service elasticsearch start
+export JAVA_HOME=/usr/lib/jvm/java-7-openjdk-amd64/
+source /.gvm/bin/gvm-init.sh
+cd /opt/ocelli/grails-ocelli
+grails run-app
