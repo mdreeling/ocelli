350c0b5e0a399089097448f7fe027ca17d796c9d
Michael Dreeling <mdreeling@gmail.com>
2014-08-20 23:05:28 -0700

    Updated config

--- start-server.sh
+++ start-server.sh
@@ -2,6 +2,7 @@
 sudo service nginx start&
 sudo service elasticsearch start&
 export JAVA_HOME=/usr/lib/jvm/java-7-openjdk-amd64/
+java -javaagent:config/newrelic/newrelic.jar -jar /opt/ocelli/bin/ocelli-server-0.0.1-SNAPSHOT.jar server /opt/ocelli/ocelli-server.yml&
 source /.gvm/bin/gvm-init.sh
 cd /opt/ocelli/grails-ocelli
 grails run-app
